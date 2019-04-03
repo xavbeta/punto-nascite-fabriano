236181f9ddf2e9eb1e089494df637ed5b326b398
Saverio Delpriori <saverio.delpriori@uniurb.it>
2019-04-03 15:17:01 +0200

    Add new extimates to PN city centers

--- run.py
+++ run.py
@@ -7,7 +7,6 @@ from typing import Any
 
 gmaps = googlemaps.Client(key='AIzaSyCemUwaxKlYav7kD-aOkjKYKlSzLGfX-xE')
 
-
 def journey_duration(origin, target, when):
     time = 0
     try:
@@ -18,7 +17,7 @@ def journey_duration(origin, target, when):
                                          )
         time = int(directions_result[0]['legs'][0]['duration']['value'] / 60)
     except:
-        pass
+        print("ERROR: {0} - {1}".format(origin, target))
 
     return time
 
@@ -33,46 +32,37 @@ pprint(fab_data)
 
 now = datetime.now()
 durations = []
-alt_durations = []
+durations_city_center = []
+alt_durations_city_center = []
+alt_durations_hospital = []
 for index, row in fab_data.iterrows():
     city_origin = row['comune']
     hospital_target = pns[pns['pn'] == 'Fabriano'].iloc[0]['address']
 
     duration = journey_duration(city_origin,hospital_target, now)
     durations.append(duration)
+
+    duration_city_center = journey_duration(city_origin, 'Fabriano', now)
+    durations_city_center.append(duration_city_center)
     #[0]['address']
     #print("o: {0} t: {1}".format(city_origin, hospital_target))
 
     alt_hospital_target = pns[pns['pn'] == row['pnalt']].iloc[0]['address']
+    alt_duration_hospital = journey_duration(city_origin, alt_hospital_target, now)
+    alt_durations_hospital.append(alt_duration_hospital)
 
-    alt_duration = journey_duration(city_origin, alt_hospital_target, now)
-    alt_durations.append(alt_duration)
+    alt_duration_city_center = journey_duration(city_origin, row['pnalt'], now)
+    alt_durations_city_center.append(alt_duration_city_center)
     # [0]['address']
     print("o: {0} t: {1} alt t: {2}".format(city_origin, hospital_target, alt_hospital_target))
 
 
 fab_data['actual_tempopnindice'] = durations
-fab_data['actual_tempopnalt'] = alt_durations
+fab_data['actual_tempopnindice_city_center'] = durations_city_center
+fab_data['actual_tempopnalt'] = alt_durations_hospital
+fab_data['actual_tempopnalt_city_center'] = alt_durations_city_center
 
 fab_data.to_csv('data/output.csv', sep=';', encoding='utf-8')
 
-
-# Geocoding an address
-#geocode_result = gmaps.geocode('Viale Stelluti Scala, 26, 60044 Fabriano AN, Italy')
-#print(geocode_result)
-# Look up an address with reverse geocoding
-#reverse_geocode_result = gmaps.reverse_geocode((45, 12))
-#print(reverse_geocode_result)
-# Request directions via public transit
-#now = datetime.now()
-#directions_result = gmaps.directions("Matelica",
-#                                     "Viale Stelluti Scala, 26, 60044 Fabriano AN",
-#                                     mode="driving",
-#                                     #departure_time=now
-#                                     )
-
-#print(int(directions_result[0]['legs'][0]['duration']['value'] / 60))
-
-#print(directions_result[0]['legs'][0]['duration']['value'])
 
 pprint(fab_data)
